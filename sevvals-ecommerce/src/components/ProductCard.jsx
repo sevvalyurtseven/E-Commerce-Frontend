8e046aa6739f4390e365cbf587d4e20d4d059bfd
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-05-31 08:45:28 +0300

    Ürüne tıklandıgında detay sayfasına yönlendiriyor.

--- sevvals-ecommerce/src/components/ProductCard.jsx
+++ sevvals-ecommerce/src/components/ProductCard.jsx
@@ -1,4 +1,4 @@
-import { useLocation } from "react-router-dom/cjs/react-router-dom.min";
+import { Link, useLocation } from "react-router-dom";
 import colors from "../assets/featured-posts/product-colors.png";
 
 function ProductCard({
@@ -10,28 +10,30 @@ function ProductCard({
 }) {
   const location = useLocation(); // Mevcut URL'yi almak için useLocation kullanılması
   return (
-    <div className="flex flex-col items-center text-center gap-5">
-      <img src={image} alt={title} className="w-full h-full" />
-      <h5 className="text-slate-800 text-base font-bold leading-normal tracking-wider">
-        {title}
-      </h5>
-      <p className="text-center text-neutral-500 text-sm font-bold leading-normal tracking-wider">
-        {department}
-      </p>
-      <div className="flex justify-center items-center gap-2">
-        <h5 className="text-center text-stone-300 text-base font-bold leading-normal tracking-wider">
-          ${originalPrice}
+    <Link to="/product-detail">
+      <div className="flex flex-col items-center text-center gap-5">
+        <img src={image} alt={title} className="w-full h-full" />
+        <h5 className="text-slate-800 text-base font-bold leading-normal tracking-wider">
+          {title}
         </h5>
-        <h5 className="text-center text-teal-700 text-base font-bold leading-normal tracking-wider">
-          ${discountedPrice}
-        </h5>
-      </div>
-      {location.pathname === "/shop" && (
-        <div className="flex justify-center">
-          <img src={colors} alt="product colors" />
+        <p className="text-center text-neutral-500 text-sm font-bold leading-normal tracking-wider">
+          {department}
+        </p>
+        <div className="flex justify-center items-center gap-2">
+          <h5 className="text-center text-stone-300 text-base font-bold leading-normal tracking-wider">
+            ${originalPrice}
+          </h5>
+          <h5 className="text-center text-teal-700 text-base font-bold leading-normal tracking-wider">
+            ${discountedPrice}
+          </h5>
         </div>
-      )}
-    </div>
+        {location.pathname === "/shop" && (
+          <div className="flex justify-center">
+            <img src={colors} alt="product colors" />
+          </div>
+        )}
+      </div>
+    </Link>
   );
 }
 
