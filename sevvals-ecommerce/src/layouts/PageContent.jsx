970d5217403b7a7e79d0196c7e385f2cb8221480
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-05-31 08:45:42 +0300

    Routing

--- sevvals-ecommerce/src/layouts/PageContent.jsx
+++ sevvals-ecommerce/src/layouts/PageContent.jsx
@@ -1,6 +1,7 @@
 import { Route, Switch } from "react-router-dom";
 import HomePage from "../pages/HomePage";
 import ShopPage from "../pages/ShopPage";
+import ProductDetailPage from "../pages/ProductDetailPage";
 
 function PageContent() {
   return (
@@ -11,6 +12,9 @@ function PageContent() {
         </Route>
         <Route path="/shop">
           <ShopPage />
+        </Route>
+        <Route path="/product-detail">
+          <ProductDetailPage />
         </Route>
       </Switch>
     </>
