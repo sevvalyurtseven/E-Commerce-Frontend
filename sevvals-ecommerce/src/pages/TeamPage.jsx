8617cfdcf95d7c0877019ed3578e6db35bca10d7
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-06-01 03:37:17 +0300

    Team sayfası olusturuldu

---
+++ sevvals-ecommerce/src/pages/TeamPage.jsx
@@ -0,0 +1,15 @@
+import Footer from "../layouts/Footer";
+import Header from "../layouts/Header";
+import Team from "../layouts/teampage/Team";
+
+function TeamPage() {
+  return (
+    <>
+      <Header />
+      <Team />
+      <Footer />
+    </>
+  );
+}
+
+export default TeamPage;
