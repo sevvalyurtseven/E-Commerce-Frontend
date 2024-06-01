e891ac61c119cae1d67bc7674a3f9bee8be32bf9
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-06-01 18:55:22 +0300

    flex yapisi grid ile degisti

--- sevvals-ecommerce/src/layouts/Clients.jsx
+++ sevvals-ecommerce/src/layouts/Clients.jsx
@@ -8,12 +8,18 @@ import robot from "../assets/brands/col-md-2-4.png";
 function Clients() {
   const clients = [hooli, lyft, yaprak, stripe, aws, robot];
   return (
-    <div className="w-[75%] mx-auto flex flex-col sm:flex-row sm:justify-between items-center sm:py-10 py-20 gap-10">
-      {clients.map((image, index) => (
-        <div key={index}>
-          <img src={image} />
-        </div>
-      ))}
+    <div className="flex flex-col justify-center items-center py-24 bg-[#FAFAFA] gap-8">
+      <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-6 gap-16 ">
+        {clients.map((image, index) => (
+          <div key={index}>
+            <img
+              src={image}
+              className="w-full  max-w-[100%] sm:max-w-[100%] md:max-w-[80%]"
+              alt={`client-${index}`}
+            />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
