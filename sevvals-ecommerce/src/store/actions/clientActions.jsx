5d2941a331c09a6e7e9f88cbdce85c241c2bf07b
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-06-04 05:14:43 +0300

    login ve log out durumları icin action type ve fetch constants olusturuldu. Giriş ve çıkış işlemleri için aksiyon creators oluşturuldu. giris ve cikisi yönetmek için thunk action creators olusturuldu. toastify mesajları eklendi

--- sevvals-ecommerce/src/store/actions/clientActions.jsx
+++ sevvals-ecommerce/src/store/actions/clientActions.jsx
@@ -1,3 +1,4 @@
+import { toast } from "react-toastify";
 import axiosInstance from "../../api/api";
 
 // Action Type Constants:
@@ -18,6 +19,14 @@ export const GET_ROLES_SUCCESS =
   "Roller fetch işlemi başarıyla tamamlandığında kullanılır";
 export const GET_ROLES_ERROR =
   "Roller fetch işlemi başarısız olduğunda kullanılır";
+
+//Login Action Type Constants:
+export const LOGIN_USER = "Kullanıcı girişi yapmak için kullanılır";
+export const LOGOUT_USER = "Kullanıcı cıkış yapmak için kullanılır";
+export const LOGIN_REQUEST = "Login işlemi başlatıldığında kullanılır";
+export const LOGIN_SUCCESS =
+  "Login işlemi başarıyla tamamlandığında kullanılır";
+export const LOGIN_FAILURE = "Login işlemi başarısız oldugunda kullanılır";
 
 // Action Creators:
 
@@ -55,6 +64,25 @@ export const fetchRolesError = (error) => ({
   payload: error,
 });
 
+// Login Action Creators:
+export const loginRequest = () => ({
+  type: LOGIN_REQUEST,
+});
+
+export const loginSuccess = (data) => ({
+  type: LOGIN_SUCCESS,
+  payload: data,
+});
+
+export const loginFailure = (error) => ({
+  type: LOGIN_FAILURE,
+  payload: error,
+});
+
+export const loginUser = (user) => ({ type: LOGIN_USER, payload: user });
+
+export const logoutUser = () => ({ type: LOGOUT_USER });
+
 // Async Action Creators:
 
 // Thunk Action Creator:
@@ -68,5 +96,36 @@ export const fetchRoles = () => {
     } catch (error) {
       dispatch(fetchRolesError(error.message));
     }
+  };
+};
+
+//Login Thunk Action Creator:
+
+export const userLogin = (data, history) => {
+  return (dispatch) => {
+    dispatch(loginRequest());
+    axiosInstance
+      .post("/login", data)
+      .then((response) => {
+        console.log(response.data);
+
+        dispatch(loginSuccess(response.data));
+        localStorage.setItem("token", response.data.token);
+        toast.success("Welcome back!");
+        history.push("/");
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        dispatch(loginFailure(error.response.data.message));
+        toast.error("Error occurred: " + error.response.data.message);
+      });
+  };
+};
+
+export const userLogout = () => {
+  return (dispatch) => {
+    dispatch(logoutUser());
+    localStorage.removeItem("token");
+    console.log("Logged out");
   };
 };
