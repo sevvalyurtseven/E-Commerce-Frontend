6a272568163251f780cd9e7c90b65e71991ccfd4
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-06-05 02:53:02 +0300

    En iyi 5 kategori rating değerine göre listelendi. Her kategori için bir kart oluşturuldu ve kategori bilgileri ile birlikte görüntülendi.

--- sevvals-ecommerce/src/components/ShopCard.jsx
+++ sevvals-ecommerce/src/components/ShopCard.jsx
@@ -1,15 +1,20 @@
-function ShopCard({ image, title, itemCount }) {
+import React from "react";
+import { Link } from "react-router-dom";
+
+function ShopCard({ image, title, gender, link }) {
   return (
     <div className="flex flex-col items-center text-center relative transition-transform duration-300 ease-in-out transform hover:scale-105">
-      <img
-        src={image}
-        alt={title}
-        className="w-full h-full object-cover filter brightness-75 grayscale-50"
-      />
-      <div className="absolute inset-7 flex flex-col items-center justify-center text-white font-bold text-base tracking-wider gap-3">
-        <h2 className="text-2xl">{title}</h2>
-        <p className="text-lg font-normal">{itemCount} Items</p>
-      </div>
+      <Link to={link} className="w-full h-full">
+        <img
+          src={image}
+          alt={title}
+          className="w-full h-full object-cover filter brightness-75 grayscale-50"
+        />
+        <div className="absolute inset-7 flex flex-col items-center justify-center text-white font-bold text-base tracking-wider gap-3">
+          <h2 className="text-2xl">{title.toUpperCase()}</h2>
+          <p className="text-lg font-normal">{gender}</p>
+        </div>
+      </Link>
     </div>
   );
 }
