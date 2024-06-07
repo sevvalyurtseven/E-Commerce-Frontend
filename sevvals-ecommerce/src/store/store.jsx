3868a4974ef5b15e40b955687e0f4a9769c654bf
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-06-07 19:47:46 +0300

    store güncellendi

--- sevvals-ecommerce/src/store/store.jsx
+++ sevvals-ecommerce/src/store/store.jsx
@@ -1,6 +1,8 @@
 import { legacy_createStore as createStore, applyMiddleware } from "redux";
 import { thunk } from "redux-thunk";
-import logger from "redux-logger";
+import { createLogger } from "redux-logger";
 import reducers from "./reducers/index.jsx";
 
-export const store = createStore(reducers, applyMiddleware(logger, thunk));
+const logger = createLogger();
+
+export const store = createStore(reducers, applyMiddleware(thunk, logger));
