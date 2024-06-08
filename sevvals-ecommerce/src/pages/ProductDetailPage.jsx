3afc9f670d5d6f1af9f6799e0723955b4352f31a
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-06-08 06:27:02 +0300

    ilgili parametreler gönderildi

--- sevvals-ecommerce/src/pages/ProductDetailPage.jsx
+++ sevvals-ecommerce/src/pages/ProductDetailPage.jsx
@@ -1,3 +1,4 @@
+import { useParams } from "react-router-dom";
 import Clients from "../layouts/Clients";
 import Footer from "../layouts/Footer";
 import Header from "../layouts/Header";
@@ -6,10 +7,25 @@ import ProductDescription from "../layouts/productdetailpage/ProductDescription"
 import ProductDetail from "../layouts/productdetailpage/ProductDetail";
 
 function ProductDetailPage() {
+  const { gender, categoryName, categoryId, productNameSlug, productId } =
+    useParams();
+  console.log("ProductDetail Page params:", {
+    gender,
+    categoryName,
+    categoryId,
+    productNameSlug,
+    productId,
+  }); // Parametrelerin doğru geçtiğini kontrol edin
   return (
     <>
       <Header />
-      <ProductDetail />
+      <ProductDetail
+        gender={gender}
+        categoryName={categoryName}
+        categoryId={categoryId}
+        productNameSlug={productNameSlug}
+        productId={productId}
+      />
       <ProductDescription />
       <BestSellerProducts />
       <Clients />
