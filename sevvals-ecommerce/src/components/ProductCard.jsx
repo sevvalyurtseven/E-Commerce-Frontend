a63b16a60962536e692c4afe26e27bf7ed9d77b0
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-06-08 06:25:45 +0300

    Ürün kartlarındaki Link kaldırıldı ve handleProductClick fonksiyonu ile yönlendirme sağlandı

--- sevvals-ecommerce/src/components/ProductCard.jsx
+++ sevvals-ecommerce/src/components/ProductCard.jsx
@@ -9,41 +9,36 @@ function ProductCard({
   originalPrice,
   discountedPrice,
 }) {
-  const location = useLocation(); // Mevcut URL'yi almak için useLocation kullanılması
+  const location = useLocation();
   return (
-    <Link
-      to="/product-detail"
-      className="transition-transform duration-300 ease-in-out transform hover:scale-110"
-    >
-      <div className="flex flex-col items-center text-center gap-5 bg-white border border-gray-100 rounded-md shadow-md pb-6 h-full">
-        <div className="flex-grow flex items-center justify-center overflow-hidden">
-          <img src={image} alt={title} className="object-contain w-full" />
-        </div>
-        <div className="flex-grow flex flex-col justify-between">
-          <div>
-            <h5 className="text-slate-800 text-base font-bold leading-normal tracking-wider line-clamp-2">
-              {title}
-            </h5>
-            <p className="text-center text-neutral-500 text-sm font-bold leading-normal tracking-wider line-clamp-2">
-              {department}
-            </p>
-          </div>
-          <div className="flex justify-center items-center gap-2">
-            <h5 className="text-center text-stone-300 text-base font-bold leading-normal tracking-wider">
-              ${originalPrice}
-            </h5>
-            <h5 className="text-center text-teal-700 text-base font-bold leading-normal tracking-wider">
-              ${discountedPrice}
-            </h5>
-          </div>
-        </div>
-        {location.pathname === "/shop" && (
-          <div className="flex justify-center">
-            <img src={colors} alt="product colors" />
-          </div>
-        )}
+    <div className="flex flex-col items-center text-center gap-5 bg-white border border-gray-100 rounded-md shadow-md pb-6 h-full">
+      <div className="flex-grow flex items-center justify-center overflow-hidden">
+        <img src={image} alt={title} className="object-contain w-full" />
       </div>
-    </Link>
+      <div className="flex-grow flex flex-col justify-between">
+        <div>
+          <h5 className="text-slate-800 text-base font-bold leading-normal tracking-wider line-clamp-2">
+            {title}
+          </h5>
+          <p className="text-center text-neutral-500 text-sm font-bold leading-normal tracking-wider line-clamp-2">
+            {department}
+          </p>
+        </div>
+        <div className="flex justify-center items-center gap-2">
+          <h5 className="text-center text-stone-300 text-base font-bold leading-normal tracking-wider">
+            ${originalPrice}
+          </h5>
+          <h5 className="text-center text-teal-700 text-base font-bold leading-normal tracking-wider">
+            ${discountedPrice}
+          </h5>
+        </div>
+      </div>
+      {location.pathname === "/shop" && (
+        <div className="flex justify-center">
+          <img src={colors} alt="product colors" />
+        </div>
+      )}
+    </div>
   );
 }
 
