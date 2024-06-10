d1f6c0f804a4472ed98e5914191c029f8a917d8d
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-06-11 00:46:58 +0300

    order sayfasındaki order summary box icin iyilestirme yapıldı. (ürün ekleme cıkarma özelliği)

--- sevvals-ecommerce/src/components/OrderSummary.jsx
+++ sevvals-ecommerce/src/components/OrderSummary.jsx
@@ -1,7 +1,14 @@
 import { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
-import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import { useSelector, useDispatch } from "react-redux";
+import { Link, useLocation } from "react-router-dom";
 import { toast } from "react-toastify";
+import {
+  decreaseQuantity,
+  increaseQuantity,
+  removeItem,
+} from "../store/actions/shoppingCartActions";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
 function OrderSummary() {
   const cart = useSelector((state) => state.shoppingCart.cart);
@@ -13,6 +20,9 @@ function OrderSummary() {
   const [appliedDiscountCode, setAppliedDiscountCode] = useState("");
   const [discountAmount, setDiscountAmount] = useState(0);
   const [isDiscountApplied, setIsDiscountApplied] = useState(false);
+  const location = useLocation(); // Mevcut yolu kontrol etmek için useLocation kullanıyoruz
+  const dispatch = useDispatch(); // dispatch fonksiyonunu kullanmak için ekledik
+
   // İndirim kodunu uygulama fonksiyonu
   const handleApplyDiscountCode = () => {
     if (
@@ -43,6 +53,7 @@ function OrderSummary() {
       .filter((item) => item.checked)
       .reduce((total, item) => total + item.count, 0);
   };
+
   // Toplam fiyat ve toplam miktar için state
   const [totalPrice, setTotalPrice] = useState(
     parseFloat(calculateTotalPrice())
@@ -73,11 +84,78 @@ function OrderSummary() {
     ? totalPrice - discountAmount
     : totalPrice + shippingCost - discountAmount;
 
+  // Ürün miktarını artırma fonksiyonu
+  const handleIncrease = (productId) => {
+    dispatch(increaseQuantity(productId));
+  };
+
+  // Ürün miktarını azaltma fonksiyonu
+  const handleDecrease = (productId) => {
+    dispatch(decreaseQuantity(productId));
+  };
+
+  // Sepetten ürün kaldırma fonksiyonu
+  const handleRemove = (productId) => {
+    dispatch(removeItem(productId));
+  };
+
   return (
     <div className="bg-blue-100 shadow-md rounded-lg p-4 text-center sticky top-4">
       <h3 className="text-xl font-bold mb-4 text-blue-700 tracking-widest">
         Order Summary
       </h3>
+      {location.pathname === "/order" && (
+        <div className="text-left my-6 max-h-48 overflow-y-auto">
+          <ul>
+            {cart.map((item) => (
+              <li
+                key={item.product.id}
+                className="mb-2 bg-white p-2 rounded-lg shadow"
+              >
+                <div className="flex justify-between items-center">
+                  <div className="flex items-center w-1/3">
+                    <img
+                      src={item.product.images[0].url}
+                      alt={item.product.name}
+                      className="w-14 h-18 object-cover mr-2"
+                    />
+                    <div className="flex flex-col">
+                      <p className="font-bold text-sm">{item.product.name}</p>
+                      <p className="text-sm">Adet: {item.count}</p>
+                    </div>
+                  </div>
+                  <div className="flex items-center justify-center w-1/3">
+                    <button
+                      onClick={() => handleDecrease(item.product.id)}
+                      className="btn btn-outline text-sky-500 btn-sm mx-1"
+                    >
+                      -
+                    </button>
+                    <span className="mx-2">{item.count}</span>
+                    <button
+                      onClick={() => handleIncrease(item.product.id)}
+                      className="btn btn-outline text-sky-500 btn-sm mx-1"
+                    >
+                      +
+                    </button>
+                  </div>
+                  <div className="flex items-center justify-end w-1/3">
+                    <p className="font-bold text-red-500 text-sm mr-2 whitespace-nowrap">
+                      {(item.product.price * item.count).toFixed(2)} TL
+                    </p>
+                    <button
+                      onClick={() => handleRemove(item.product.id)}
+                      className="btn btn-sm"
+                    >
+                      <FontAwesomeIcon icon={faTrash} />
+                    </button>
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
       <div className="mb-4 space-y-2 text-left tracking-wider">
         <p className="flex justify-between text-blue-600 font-medium">
           <span>Total Quantity:</span>
@@ -125,7 +203,7 @@ function OrderSummary() {
       </div>
       <Link
         to="/order"
-        className="btn btn-primary w-full bg-blue-500 hover:bg-blue-700 text-sm tracking-widest"
+        className="btn btn-primary w-full bg-blue-500 hover:bg-blue-700 text-sm tracking-widest mt-4"
       >
         Create Order
       </Link>
