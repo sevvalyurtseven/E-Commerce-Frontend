f8b612b9e39874a22c37dcbd5c30558c1267e6e6
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-06-11 19:04:53 +0300

    config dosyası düzenlendi

--- sevvals-ecommerce/vite.config.js
+++ sevvals-ecommerce/vite.config.js
@@ -1,15 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://workintech-fe-ecommerce.onrender.com",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
-  },
 });
