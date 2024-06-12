115209e134a9eae984f9a99b182eec2b42bc2e79
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-06-12 04:02:08 +0300

    Order page css detail

--- sevvals-ecommerce/src/layouts/orderpage/Order.jsx
+++ sevvals-ecommerce/src/layouts/orderpage/Order.jsx
@@ -6,6 +6,8 @@ import {
   removeAddress,
 } from "../../store/actions/shoppingCartActions";
 import OrderSummary from "../../components/OrderSummary";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
 function Order() {
   const dispatch = useDispatch();
@@ -13,6 +15,7 @@ function Order() {
   const [isEditing, setIsEditing] = useState(false);
   const [editAddressId, setEditAddressId] = useState(null);
   const [showForm, setShowForm] = useState(false);
+  const [showAddresses, setShowAddresses] = useState(false);
   const [activeTab, setActiveTab] = useState("address");
   const [selectedAddress, setSelectedAddress] = useState(null);
 
@@ -40,6 +43,7 @@ function Order() {
 
   const handleAddressSelection = (addressId) => {
     setSelectedAddress(addressId);
+    setShowAddresses(false);
   };
 
   const handleTabClick = (tab) => {
@@ -47,9 +51,9 @@ function Order() {
   };
 
   return (
-    <div className="flex flex-col lg:flex-row justify-between mx-auto max-w-screen-xl py-10">
-      <div className="w-full lg:w-8/12 mx-auto bg-white shadow-lg rounded-lg p-4">
-        <div className="flex border-b mb-4">
+    <div className="flex flex-col lg:flex-row justify-between mx-auto max-w-screen-xl py-10 min-h-full">
+      <div className="w-full lg:w-8/12 mx-auto bg-[#fafafa] shadow-lg rounded-lg p-4">
+        <div className="flex border-b mb-4 text-xl tracking-widest font-bold">
           <button
             className={`w-1/2 py-2 text-center ${
               activeTab === "address" ? "border-b-2 border-blue-500" : ""
@@ -70,17 +74,16 @@ function Order() {
         </div>
 
         {activeTab === "address" && (
-          <div>
-            <h2 className="text-2xl font-bold mb-4">Adres Bilgileri</h2>
+          <div className="flex flex-col h-[90%] space-y-4">
             <button
-              className="btn btn-primary mb-4"
+              className="btn bg-blue-500 hover:bg-blue-700 text-white w-full flex-grow shadow-lg rounded-lg text-3xl tracking-widest"
               onClick={() => setShowForm(true)}
             >
-              Adres Ekle
+              Yeni Adres Ekle <FontAwesomeIcon icon={faPlus} />
             </button>
             {showForm && (
               <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-                <div className="bg-white rounded-lg shadow-lg p-8 w-full max-w-md">
+                <div className="bg-white rounded-lg shadow-lg p-8 w-full max-w-md relative">
                   <button
                     className="absolute top-2 right-2 text-gray-600"
                     onClick={() => setShowForm(false)}
@@ -104,42 +107,65 @@ function Order() {
                 </div>
               </div>
             )}
-            <h3 className="text-xl font-bold mt-8">Kayıtlı Adresler</h3>
-            <ul className="space-y-4">
-              {addresses.map((address) => (
-                <li
-                  key={address.id}
-                  className={`border p-4 rounded-lg ${
-                    selectedAddress === address.id ? "bg-gray-200" : ""
-                  }`}
-                  onClick={() => handleAddressSelection(address.id)}
-                >
-                  <h4 className="font-bold">{address.title}</h4>
-                  <p>
-                    {address.name} {address.surname}
-                  </p>
-                  <p>{address.phone}</p>
-                  <p>
-                    {address.city}, {address.district}, {address.neighborhood}
-                  </p>
-                  <p>{address.address}</p>
-                  <div className="space-x-2 mt-2">
-                    <button
-                      onClick={() => handleEditClick(address)}
-                      className="btn btn-secondary btn-sm"
-                    >
-                      Düzenle
-                    </button>
-                    <button
-                      onClick={() => handleDeleteClick(address.id)}
-                      className="btn btn-error btn-sm"
-                    >
-                      Sil
-                    </button>
-                  </div>
-                </li>
-              ))}
-            </ul>
+            <button
+              className="btn bg-blue-500 hover:bg-blue-700 text-white w-full flex-grow shadow-lg rounded-lg text-3xl tracking-widest"
+              onClick={() => setShowAddresses(true)}
+            >
+              Kayıtlı Adreslerim
+            </button>
+            {showAddresses && (
+              <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+                <div className="bg-white rounded-lg shadow-lg p-8 w-full max-w-md relative">
+                  <button
+                    className="absolute top-2 right-2 text-gray-600"
+                    onClick={() => setShowAddresses(false)}
+                  >
+                    &times;
+                  </button>
+                  <h3 className="text-xl font-bold mb-4">Kayıtlı Adreslerim</h3>
+                  {addresses.length > 0 ? (
+                    <ul className="space-y-4">
+                      {addresses.map((address) => (
+                        <li
+                          key={address.id}
+                          className={`border p-4 rounded-lg cursor-pointer ${
+                            selectedAddress === address.id ? "bg-gray-200" : ""
+                          }`}
+                          onClick={() => handleAddressSelection(address.id)}
+                        >
+                          <h4 className="font-bold">{address.title}</h4>
+                          <p>
+                            {address.name} {address.surname}
+                          </p>
+                          <p>{address.phone}</p>
+                          <p>
+                            {address.city}, {address.district},{" "}
+                            {address.neighborhood}
+                          </p>
+                          <p>{address.address}</p>
+                          <div className="space-x-2 mt-2">
+                            <button
+                              onClick={() => handleEditClick(address)}
+                              className="btn btn-secondary btn-sm shadow-lg rounded-lg"
+                            >
+                              Düzenle
+                            </button>
+                            <button
+                              onClick={() => handleDeleteClick(address.id)}
+                              className="btn btn-error btn-sm shadow-lg rounded-lg"
+                            >
+                              Sil
+                            </button>
+                          </div>
+                        </li>
+                      ))}
+                    </ul>
+                  ) : (
+                    <p>Kayıtlı adres bulunmamaktadır.</p>
+                  )}
+                </div>
+              </div>
+            )}
           </div>
         )}
 
