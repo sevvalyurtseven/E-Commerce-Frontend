467026695d61fe1f2ad9fdbe0e91c4fa12246e44
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-06-12 11:12:43 +0300

    Ödeme yöntemlerini yönetmek için action type constants  ve action creatorlar olusturuldu

--- sevvals-ecommerce/src/store/actions/shoppingCartActions.jsx
+++ sevvals-ecommerce/src/store/actions/shoppingCartActions.jsx
@@ -3,7 +3,10 @@ import axiosInstance from "../../api/api";
 // Action Type Constants:
 
 export const SET_CART = "Sepet içeriğini ayarlar";
-export const SET_PAYMENT = "Ödeme bilgilerini ayarlar";
+export const SET_PAYMENT_METHODS = "Ödeme yöntemlerini ayarlar";
+export const ADD_PAYMENT_METHOD = "Ödeme yöntemini ekleme işlemi";
+export const UPDATE_PAYMENT_METHOD = "Ödeme yöntemini güncelleme işlemi";
+export const DELETE_PAYMENT_METHOD = "Ödeme yöntemini silme işlemi";
 export const SET_ADDRESS = "Adres bilgilerini ayarlar";
 export const ADD_ADDRESS = "Adres ekleme işlemi";
 export const UPDATE_ADDRESS = "Adres bilgilerini güncelleme işlemi";
@@ -33,9 +36,24 @@ export const setCart = (cart) => ({
   payload: cart,
 });
 
-export const setPayment = (payment) => ({
-  type: SET_PAYMENT,
-  payload: payment,
+export const setPaymentMethods = (methods) => ({
+  type: SET_PAYMENT_METHODS,
+  payload: methods,
+});
+
+export const addPaymentMethod = (method) => ({
+  type: ADD_PAYMENT_METHOD,
+  payload: method,
+});
+
+export const updatePaymentMethod = (method) => ({
+  type: UPDATE_PAYMENT_METHOD,
+  payload: method,
+});
+
+export const deletePaymentMethod = (methodId) => ({
+  type: DELETE_PAYMENT_METHOD,
+  payload: methodId,
 });
 
 export const setAddress = (address) => ({
