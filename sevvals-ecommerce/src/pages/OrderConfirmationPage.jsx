3baac3d8675bfc31fb1adb65555228fa68ae1706
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-06-13 06:00:07 +0300

    page template

---
+++ sevvals-ecommerce/src/pages/OrderConfirmationPage.jsx
@@ -0,0 +1,15 @@
+import Footer from "../layouts/Footer";
+import Header from "../layouts/Header";
+import OrderConfirmation from "../layouts/confirmpage/OrderConfirmation";
+
+function OrderConfirmationPage() {
+  return (
+    <>
+      <Header />
+      <OrderConfirmation />
+      <Footer />
+    </>
+  );
+}
+
+export default OrderConfirmationPage;
