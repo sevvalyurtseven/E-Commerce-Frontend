092bf318c79ed0d804a95741056a10a71a21be59
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-06-13 16:38:26 +0300

    icerik ingilizceye cevrildi

--- sevvals-ecommerce/src/components/PreviousOrders.jsx
+++ sevvals-ecommerce/src/components/PreviousOrders.jsx
@@ -49,27 +49,27 @@ const PreviousOrders = () => {
     let shippingDate;
 
     if (isThursday(currentDate)) {
-      shippingDate = addDays(currentDate, 4); // Perşembe günü verilen siparişler pazartesi kargoya verilir
+      shippingDate = addDays(currentDate, 4); // Orders placed on Thursday are shipped on Monday
     } else if (
       isFriday(currentDate) ||
       isSaturday(currentDate) ||
       isSunday(currentDate)
     ) {
-      shippingDate = addDays(currentDate, 3); // Cuma, cumartesi veya pazar günü verilen siparişler pazartesi kargoya verilir
+      shippingDate = addDays(currentDate, 3); // Orders placed on Friday, Saturday, or Sunday are shipped on Monday
     } else {
-      shippingDate = addDays(currentDate, 2); // Diğer günlerde 2 gün sonra kargoya verilir
+      shippingDate = addDays(currentDate, 2); // Orders placed on other days are shipped 2 days later
     }
 
     return format(shippingDate, "EEEE, MMMM d");
   };
 
   return (
-    <div className="container mx-auto px-4 py-8 bg-gray-50 min-h-screen">
+    <div className="container mx-auto px-4 py-8 bg-white min-h-screen">
       <h1 className="text-5xl font-extrabold mb-8 text-center text-gray-900">
-        Önceki Siparişlerim
+        My Previous Orders
       </h1>
       {isFetching ? (
-        <p className="text-center text-gray-600">Yükleniyor...</p>
+        <p className="text-center text-gray-600">Loading...</p>
       ) : (
         <div className="space-y-8">
           {orders.map((order) => {
@@ -93,16 +93,16 @@ const PreviousOrders = () => {
                 key={order.id}
                 trigger={
                   <div
-                    className="order-card bg-white shadow-md hover:shadow-lg transition-shadow duration-300 cursor-pointer p-6 rounded-xl flex justify-between items-center border border-gray-200"
+                    className="order-card bg-gray-50 shadow-md hover:shadow-lg transition-shadow duration-300 cursor-pointer p-6 rounded-lg flex justify-between items-center border border-gray-200"
                     onClick={() => handleTriggerClick(order.id)}
                   >
                     <div className="flex items-center gap-4">
                       <FontAwesomeIcon
                         icon={faShoppingBag}
-                        className="text-3xl text-purple-500"
+                        className="text-3xl text-indigo-600"
                       />
                       <span className="text-xl font-semibold text-gray-800">
-                        Sipariş ID: {order.id}
+                        Order ID: {order.id}
                       </span>
                     </div>
                     <div className="flex items-center gap-4">
@@ -117,48 +117,48 @@ const PreviousOrders = () => {
                   </div>
                 }
                 open={openOrderId === order.id}
-                className="shadow-lg rounded-xl overflow-hidden"
-                openedClassName="shadow-2xl rounded-xl overflow-hidden"
+                className="shadow-lg rounded-lg overflow-hidden"
+                openedClassName="shadow-2xl rounded-lg overflow-hidden"
               >
-                <div className="p-8 bg-white border-t-4 border-purple-500 rounded-b-xl">
+                <div className="p-8 bg-white border-t-4 border-indigo-600 rounded-b-lg">
                   <h2 className="text-3xl font-semibold mb-6 text-gray-800 text-center">
-                    Sipariş Özeti
+                    Order Summary
                   </h2>
                   <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mb-6">
-                    <div className="bg-gray-50 p-6 rounded-xl shadow-md">
+                    <div className="bg-gray-50 p-6 rounded-lg shadow-md">
                       <h3 className="text-2xl font-semibold mb-4 text-gray-700">
-                        Adres Bilgileri
+                        Address Information
                       </h3>
                       {address ? (
                         <>
                           <p className="text-gray-700 mb-2 text-lg">
-                            <strong>Başlık:</strong> {address.title}
+                            <strong>Title:</strong> {address.title}
                           </p>
                           <p className="text-gray-700 mb-2 text-lg">
-                            <strong>Ad-Soyad:</strong> {address.name}{" "}
+                            <strong>Name:</strong> {address.name}{" "}
                             {address.surname}
                           </p>
                           <p className="text-gray-700 mb-2 text-lg">
-                            <strong>Telefon:</strong> {address.phone}
+                            <strong>Phone:</strong> {address.phone}
                           </p>
                           <p className="text-gray-700 mb-2 text-lg">
-                            <strong>Şehir, İlçe, Mahalle:</strong>{" "}
+                            <strong>City, District, Neighborhood:</strong>{" "}
                             {address.city}, {address.district},{" "}
                             {address.neighborhood}
                           </p>
                           <p className="text-gray-700 text-lg">
-                            <strong>Adres Detayları:</strong> {address.address}
+                            <strong>Address Details:</strong> {address.address}
                           </p>
                         </>
                       ) : (
                         <p className="text-gray-700 text-lg">
-                          Adres bilgisi bulunamadı.
+                          Address information not found.
                         </p>
                       )}
                     </div>
-                    <div className="bg-gray-50 p-6 rounded-xl shadow-md">
+                    <div className="bg-gray-50 p-6 rounded-lg shadow-md">
                       <h3 className="text-2xl font-semibold mb-4 text-gray-700">
-                        Ürünler
+                        Products
                       </h3>
                       {order.products.map((item) => (
                         <div
@@ -175,7 +175,7 @@ const PreviousOrders = () => {
                               {item.name}
                             </h4>
                             <p className="text-sm text-gray-600">
-                              Adet: {item.count}
+                              Quantity: {item.count}
                             </p>
                           </div>
                           <span className="text-lg text-gray-800">
@@ -185,35 +185,35 @@ const PreviousOrders = () => {
                       ))}
                     </div>
                   </div>
-                  <div className="bg-gray-50 p-6 rounded-xl shadow-md mb-6">
+                  <div className="bg-gray-50 p-6 rounded-lg shadow-md mb-6">
                     <h3 className="text-2xl font-semibold mb-4 text-gray-700">
-                      Genel Toplam
+                      Total Amount
                     </h3>
                     <p className="text-gray-700 mb-2 text-lg">
-                      <strong>Toplam Tutar:</strong> {order.price} TL
+                      <strong>Total Price:</strong> {order.price} TL
                     </p>
                     {shippingCost > 0 && (
                       <p className="text-gray-700 mb-2 text-lg">
-                        <strong>Kargo Ücreti:</strong> {shippingCost} TL
+                        <strong>Shipping Cost:</strong> {shippingCost} TL
                       </p>
                     )}
                     {discountAmount > 0 && (
                       <p className="text-gray-700 mb-2 text-lg">
-                        <strong>İndirim:</strong> -{discountAmount.toFixed(2)}{" "}
+                        <strong>Discount:</strong> -{discountAmount.toFixed(2)}{" "}
                         TL
                       </p>
                     )}
                     <p className="text-gray-700 text-lg">
-                      <strong>Genel Toplam:</strong>{" "}
-                      {finalTotalPrice.toFixed(2)} TL
+                      <strong>Final Total:</strong> {finalTotalPrice.toFixed(2)}{" "}
+                      TL
                     </p>
                   </div>
-                  <div className="bg-gray-50 p-6 rounded-xl shadow-md">
+                  <div className="bg-gray-50 p-6 rounded-lg shadow-md">
                     <h3 className="text-2xl font-semibold mb-4 text-gray-700">
-                      Teslimat Zamanı
+                      Delivery Time
                     </h3>
                     <p className="text-gray-700 text-lg">
-                      <strong>Tahmini Kargo Tarihi:</strong> {shippingDate}
+                      <strong>Estimated Shipping Date:</strong> {shippingDate}
                     </p>
                   </div>
                 </div>
